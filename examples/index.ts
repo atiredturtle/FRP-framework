9916975ed79ebb1924d25eb2306cec3db47f2c91
atiredturtle <guy@segev.com>
2020-08-12 11:32:00 +1000

    starts drawSandbox

--- examples/index.ts
+++ examples/index.ts
@@ -1,63 +1,68 @@
-import { Time, constB, timeB, lift, main } from "../src";
-
-type Rect = {
-  x: number;
-  y: number;
-  color: string;
-  width: number;
-  height: number;
-};
-const draw = (ctx: CanvasRenderingContext2D, rect: Rect) => {
-  const oldFillStyle = ctx.fillStyle;
-  const { x, y, color, width, height } = rect;
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, width, height);
-
-  // restore old fillStyle
-  ctx.fillStyle = oldFillStyle;
-};
+import {
+  Time,
+  Rect,
+  drawSandbox,
+  constB,
+  timeB,
+  lift,
+  main,
+  Scene,
+} from "../src";
 
 const WIDTH = 700;
 const HEIGHT = 500;
 
 const getCharacterPos = lift((t) => ({ x: t, y: 0 }), timeB);
 
-type State = {
-  characterPos: { x: number; y: number };
+const canvasBackground: Rect = {
+  x: 0,
+  y: 0,
+  color: "black",
+  width: WIDTH,
+  height: HEIGHT,
 };
-const getState = (t: Time): State => {
-  return {
-    characterPos: getCharacterPos(t),
-  };
-};
+drawSandbox(
+  (t: Time): Scene => {
+    const { x, y } = getCharacterPos(t);
+    return [canvasBackground, { x, y, color: "red", width: 50, height: 100 }];
+  }
+);
 
+// type State = {
+//   characterPos: { x: number; y: number };
+// };
+// const getState = (t: Time): State => {
+//   return {
+//     characterPos: getCharacterPos(t),
+//   };
+// };
 // run program
-main(getState, (state: State) => {
-  // get canvas
-  const canvas = <HTMLCanvasElement>document.getElementById("myCanvas");
-  canvas.width = WIDTH;
-  canvas.height = HEIGHT;
-  const ctx = canvas.getContext("2d");
-  if (!ctx) throw Error("no ctx found");
-  const canvasBackground: Rect = {
-    x: 0,
-    y: 0,
-    color: "black",
-    width: WIDTH,
-    height: HEIGHT,
-  };
-
-  // draw background
-  draw(ctx, canvasBackground);
-
-  // actually draw based on state
-  const { characterPos } = state;
-  // draw character
-  draw(ctx, {
-    x: characterPos.x,
-    y: characterPos.y,
-    color: "red",
-    width: 50,
-    height: 100,
-  });
-});
+// main(getState, (state: State) => {
+//   // get canvas
+//   const canvas = <HTMLCanvasElement>document.getElementById("myCanvas");
+//   canvas.width = WIDTH;
+//   canvas.height = HEIGHT;
+//   const ctx = canvas.getContext("2d");
+//   if (!ctx) throw Error("no ctx found");
+//   const canvasBackground: Rect = {
+//     x: 0,
+//     y: 0,
+//     color: "black",
+//     width: WIDTH,
+//     height: HEIGHT,
+//   };
+//
+//   // draw background
+//   draw(ctx, canvasBackground);
+//
+//   // actually draw based on state
+//   const { characterPos } = state;
+//   // draw character
+//   draw(ctx, {
+//     x: characterPos.x,
+//     y: characterPos.y,
+//     color: "red",
+//     width: 50,
+//     height: 100,
+//   });
+// });
--- src/index.ts
+++ src/index.ts
@@ -41,3 +41,123 @@ export const main = <A>(b: Behavior<A>, cb: (a: A) => void): void => {
     time++;
   }, 10);
 };
+
+// TODO: should take in scene
+const draw = (ctx: CanvasRenderingContext2D, rect: Rect) => {
+  const oldFillStyle = ctx.fillStyle;
+  const { x, y, color, width, height } = rect;
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+
+  // restore old fillStyle
+  ctx.fillStyle = oldFillStyle;
+};
+
+export type Rect = {
+  x: number;
+  y: number;
+  color: string;
+  width: number;
+  height: number;
+};
+export type Shapes = Rect;
+export type Scene = Shapes[];
+
+// draw sandbox
+// take in Behavior<Scene>
+// sets up a canvas and renders the Scene
+// also sets up the pause buttons etc to control time
+
+const CANVAS_WIDTH = 700;
+const CANVAS_HEIGHT = 500;
+export const drawSandbox = (animation: Behavior<Scene>) => {
+  // ======CREATES DOM ELEMENTS======
+  const sandboxDiv = document.createElement("div");
+  sandboxDiv.setAttribute("id", "sandbox");
+
+  // set up canvas
+  const canvas = document.createElement("canvas");
+  // set up buttons
+  const playButton = document.createElement("button");
+  playButton.appendChild(document.createTextNode("Play"));
+
+  const pauseButton = document.createElement("button");
+  pauseButton.appendChild(document.createTextNode("Pause"));
+
+  // set up time counter
+  const timeCounter = document.createElement("div");
+
+  // set up slider
+  sandboxDiv.append(canvas, playButton, pauseButton, timeCounter);
+
+  // add the newly created element and its content into the DOM
+  document.body.appendChild(sandboxDiv);
+
+  canvas.width = CANVAS_WIDTH;
+  canvas.height = CANVAS_HEIGHT;
+
+  // draw scene
+  type State = {
+    time: Time;
+    scene: Scene;
+  };
+  const getState = (t: Time): State => ({
+    time: timeB(t),
+    scene: animation(t),
+  });
+  const { play, pause } = mainSandbox(getState, (state: State) => {
+    const { time, scene } = state;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) throw Error("no ctx found");
+    scene.forEach((r) => draw(ctx, r));
+    console.log("time", time);
+    // draw time
+    timeCounter.innerText = `time: ${time}`;
+  });
+
+  // adds play + pause callbacks to buttons
+  playButton.addEventListener("click", () => {
+    console.log("play");
+    play();
+  });
+  pauseButton.addEventListener("click", () => {
+    console.log("pause");
+    pause();
+  });
+};
+
+interface MainSandbox {
+  play: () => void;
+  pause: () => void;
+}
+export const mainSandbox = <A>(
+  b: Behavior<A>,
+  cb: (a: A) => void
+): MainSandbox => {
+  let time = 0;
+  let paused = true;
+  // runs interval to provide time
+  setInterval(() => {
+    // console.log(`time: ${time}`);
+    const value = b(time);
+    cb(value);
+
+    // increment time
+    if (!paused) {
+      time++;
+    }
+  }, 10);
+
+  // controls time
+  const play = () => {
+    paused = false;
+  };
+  const pause = () => {
+    paused = true;
+  };
+
+  return {
+    play,
+    pause,
+  };
+};
